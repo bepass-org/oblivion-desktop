95d53f6f45b8585c54686975ca4a8d655d245ff2
IRCF <ircfspace@gmail.com>
2024-04-13 13:48:47 +0330

    تکمیل طراحی صفحه درباره

--- src/renderer/pages/About.tsx
+++ src/renderer/pages/About.tsx
@@ -11,8 +11,50 @@ export default function About() {
                 "normalPage"
             )}>
                 <div className="container">
-                    <p className="dirLeft">
-                        version: {packageJsonData.version}
+                    <p>این‌برنامه یک نسخه غیررسمی، اما قابل اطمینان از اپ Oblivion یا فراموشی است که برای ویندوز، لینوکس و مک ارائه گردیده است.
+                        <br />
+                        برنامه Oblivion Desktop با الگو گرفتن از رابط کاربری نسخه اصلی که توسط یوسف قبادی برنامه‌نویسی شده بود، با هدف دسترسی آزاد به اینترنت تهیه گردیده و هرگونه تغییر نام یا استفاده تجاری از آن مجاز نمی‌باشد.</p>
+                    <p><b>اینترنت برای همه، یا هیچ‌کس!</b></p>
+                    <div className="socialMedia">
+                        <a href="https://github.com/bepass-org/oblivion" target="_blank" rel="noreferrer">
+                            <div className="item">
+                                <div className="icon">
+                                    <img src="../../../assets/img/github-mark.png" alt="github" />
+                                </div>
+                                <div className="host">Github</div>
+                                <div className="name">bepass-org/Oblivion</div>
+                            </div>
+                        </a>
+                        <a href="https://github.com/bepass-org/warp-plus" target="_blank" rel="noreferrer">
+                            <div className="item">
+                                <div className="icon">
+                                    <img src="../../../assets/img/github-mark.png" alt="github" />
+                                </div>
+                                <div className="host">Github</div>
+                                <div className="name">bepass-org/Warp-Plus</div>
+                            </div>
+                        </a>
+                        <a href="https://github.com/kiomarzsss/oblivion-desktop" target="_blank" rel="noreferrer">
+                            <div className="item">
+                                <div className="icon">
+                                    <img src="../../../assets/img/github-mark.png" alt="github" />
+                                </div>
+                                <div className="host">Github</div>
+                                <div className="name">kiomarzsss/Oblivion-Desktop</div>
+                            </div>
+                        </a>
+                        <a href="https://ircf.space" target="_blank" rel="noreferrer">
+                            <div className="item">
+                                <div className="icon">
+                                    <img src="../../../assets/img/ircf.png" alt="ircf" />
+                                </div>
+                                <div className="host">IRCF</div>
+                                <div className="name">ircf.space</div>
+                            </div>
+                        </a>
+                    </div>
+                    <p className="text-center">
+                        <small>App Version: <b>{packageJsonData.version}</b></small>
                     </p>
                 </div>
             </div>
