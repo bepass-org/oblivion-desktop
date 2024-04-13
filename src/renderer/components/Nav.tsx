7e7f5fc52864df0112dec7f83b538f117af9220f
IRCF <ircfspace@gmail.com>
2024-04-13 11:53:12 +0330

    smallFix

--- src/renderer/components/Nav.tsx
+++ src/renderer/components/Nav.tsx
@@ -1,25 +1,27 @@
-import BackButton from './BackButton';
 import { useEffect } from 'react';
+import BackButton from './BackButton';
 
 export default function Nav({ title }: { title: string }) {
+
+    const isSticky = () => {
+        const header = document.querySelector('nav');
+        const scrollTop = window?.scrollY;
+        if ( header ) {
+            if ( scrollTop >= 20 ) {
+                header.classList.add('isSticky')
+            }
+            else {
+                header?.classList.remove('isSticky');
+            }
+        }
+    };
 
     useEffect(() => {
         window.addEventListener('scroll', isSticky);
         return () => {
             window.removeEventListener('scroll', isSticky);
         };
-    });
-
-    const isSticky = () => {
-        const header = document.querySelector('nav');
-        const scrollTop = window.scrollY;
-        if ( scrollTop >= 20 ) {
-            header.classList.add('isSticky')
-        }
-        else {
-            header.classList.remove('isSticky');
-        }
-    };
+    }, []);
 
     return (
         <>
