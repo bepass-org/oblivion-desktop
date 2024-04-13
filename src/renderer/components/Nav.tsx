1ee7f63feb5f7af8d25fc04e55a9a734fccb577d
IRCF <ircfspace@gmail.com>
2024-04-13 11:48:54 +0330

    استیکی هدر

--- src/renderer/components/Nav.tsx
+++ src/renderer/components/Nav.tsx
@@ -1,6 +1,26 @@
 import BackButton from './BackButton';
+import { useEffect } from 'react';
 
 export default function Nav({ title }: { title: string }) {
+
+    useEffect(() => {
+        window.addEventListener('scroll', isSticky);
+        return () => {
+            window.removeEventListener('scroll', isSticky);
+        };
+    });
+
+    const isSticky = () => {
+        const header = document.querySelector('nav');
+        const scrollTop = window.scrollY;
+        if ( scrollTop >= 20 ) {
+            header.classList.add('isSticky')
+        }
+        else {
+            header.classList.remove('isSticky');
+        }
+    };
+
     return (
         <>
             <nav>
