a98efdf4fb053ae18183a4aa3754be4ce3ff2cb6
IRCF <ircfspace@gmail.com>
2024-04-13 20:43:35 +0330

    تغییر جزیی در استایل مودال

--- src/renderer/components/Modal.tsx
+++ src/renderer/components/Modal.tsx
@@ -7,6 +7,9 @@ export default function Modal({ title }: { title: string }) {
                 <div className="dialogBg"></div>
                 <div className="dialogBox">
                     <div className='container'>
+                        <div className="line">
+                            <div className="miniLine"></div>
+                        </div>
                         <h3>{title}</h3>
                         <input value="127.0.0.1" className="form-control" />
                         <div className="clearfix"/>
