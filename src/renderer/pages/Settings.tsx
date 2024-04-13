f7883a1402578d9e50d02a50b3fc06182a67b4cf
IRCF <ircfspace@gmail.com>
2024-04-13 13:10:26 +0330

    شروع استایل‌دهی تنظیمات

--- src/renderer/pages/Settings.tsx
+++ src/renderer/pages/Settings.tsx
@@ -1,10 +1,10 @@
 import classNames from 'classnames';
+import {useState} from "react";
 import Nav from '../components/Nav';
 import { toggleDarkMode } from '../lib/utils';
-import packageJsonData from "../../../package.json";
 
 export default function Settings() {
-    const isDarkMode = document.documentElement.getAttribute('data-bs-theme') === 'dark';
+    const [darkModeStatus, setDarkModeStatus] = useState(document.documentElement.getAttribute('data-bs-theme') === 'dark');
 
     return (
         <>
@@ -13,24 +13,25 @@ export default function Settings() {
                 "myApp",
                 "normalPage"
             )}>
-                <div className="container">
-                    <div
-        className="form-check form-switch"
-        onClick={() => {
-          toggleDarkMode();
-        }}
-      >
-        <label className="form-check-label" htmlFor="flexSwitchCheckChecked">
-          دارک‌مود:
-        </label>
-        <input
-          className="form-check-input"
-          type="checkbox"
-          role="switch"
-          id="flexSwitchCheckChecked"
-          checked={false}
-        />
-      </div>
+                <div className="settings">
+                    <div className="item">
+                        <label className="key" htmlFor="flexSwitchCheckChecked">حالت تیره</label>
+                        <div className="value">
+                            <div
+                                className={classNames(
+                                    "checkbox",
+                                    (darkModeStatus ? "checked" : ""),
+                                )}
+                                 onClick={() => {
+                                     toggleDarkMode();
+                                     setDarkModeStatus(!darkModeStatus);
+                                 }}
+                            >
+                                <i className="material-icons">&#xe876;</i>
+                            </div>
+                        </div>
+                        <div className="info" id="flexSwitchCheckChecked">مشخص‌کردن حالت نمایش برنامه</div>
+                    </div>
                 </div>
             </div>
         </>
