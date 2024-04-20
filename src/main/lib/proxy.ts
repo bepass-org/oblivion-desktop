d2a60a98bf31e32a19e6b8098a72a78a69fc38b0
kiomarzsss <kiomarzsss@gmail.com>
2024-04-20 17:26:27 +0330

    🩹 fix: windows proxy settings lib

--- src/main/lib/proxy.ts
+++ src/main/lib/proxy.ts
@@ -2,20 +2,21 @@ const { spawn } = require('child_process');
 
 const platform = process.platform; // linux / win32 / darwin / else(not supported...)
 
+// tweaking windows proxy settings using powershell
+const windowsProxySettings = (args: string[]) => {
+    spawn('powershell', [
+        'Set-ItemProperty',
+        '-Path',
+        "'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings'",
+        ...args,
+    ]);
+};
+
 export const enableProxy = () => {
     if (platform === 'win32') {
-        spawn('powershell', [
-            'Set-ItemProperty',
-            '-Path',
-            "'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings'",
-            'ProxyEnable',
-            '-value',
-            '1',
-        ]);
-        spawn('powershell', [
-            'Set-ItemProperty',
-            '-Path',
-            "'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings'",
+        windowsProxySettings(['ProxyEnable', '-value', '1']);
+        windowsProxySettings(['ProxyServer', '-value', '127.0.0.1:8086']);
+        windowsProxySettings([
             'ProxyOverride',
             '"localhost,127.*,10.*,172.16.*,172.17.*,172.18.*,172.19.*,172.20.*,172.21.*,172.22.*,172.23.*,172.24.*,172.25.*,172.26.*,172.27.*,172.28.*,172.29.*,172.30.*,172.31.*,192.168.*,<local>"',
         ]);
@@ -26,14 +27,7 @@ export const enableProxy = () => {
 
 export const disableProxy = () => {
     if (platform === 'win32') {
-        spawn('powershell', [
-            'Set-ItemProperty',
-            '-Path',
-            "'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings'",
-            'ProxyEnable',
-            '-value',
-            '0',
-        ]);
+        windowsProxySettings(['ProxyEnable', '-value', '0']);
     } else {
         console.log('not supported yet');
     }
