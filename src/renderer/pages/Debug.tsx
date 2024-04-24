53d6e5d54bfd6460bb1643a22dc0d091dad7ee23
IRCF <ircfspace@gmail.com>
2024-04-25 00:30:43 +0330

    امکان کپی کردن لاگ

--- src/renderer/pages/Debug.tsx
+++ src/renderer/pages/Debug.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import Nav from '../components/Nav';
-import { ipcRenderer } from '../lib/utils';
+import { ipcRenderer, } from '../lib/utils';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function Debug() {
     const [log, setLog] = useState('');
@@ -16,14 +17,47 @@ export default function Debug() {
         ipcRenderer.sendMessage('log');
     }, []);
 
+    const handleCopy = (e: { preventDefault: () => void }, value: any) => {
+        e.preventDefault();
+        navigator.clipboard.writeText(value);
+        toast("کپی شد!", {
+            style: {
+                fontSize: '13px',
+                borderRadius: '10px',
+                background: '#333',
+                color: '#fff',
+            },
+            duration: 2000
+        });
+    };
+
+    const handleClearLog = (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+    };
+
     return (
         <>
             <Nav title='لاگ برنامه' />
             <div className={classNames('myApp', 'normalPage', 'logPage')}>
                 <div className='container'>
                     <div className={classNames('logOptions', log === '' ? 'hidden' : '')}>
-                        <i className='material-icons'>&#xf0ff;</i>
-                        <i className='material-icons'>&#xe14d;</i>
+                        {/*<i
+                            className='material-icons'
+                            onClick={(e: any) => {
+                                handleClearLog(
+                                    e
+                                );
+                            }}
+                        >&#xf0ff;</i>*/}
+                        <i
+                            className='material-icons'
+                            onClick={(e: any) => {
+                                handleCopy(
+                                    e,
+                                    log
+                                );
+                            }}
+                        >&#xe14d;</i>
                     </div>
                     <p className={classNames(log === '' ? 'dirRight' : 'dirLeft', 'logText')}>
                         {log === ''
@@ -32,6 +66,10 @@ export default function Debug() {
                     </p>
                 </div>
             </div>
+            <Toaster
+                position='bottom-center'
+                reverseOrder={false}
+            />
         </>
     );
 }
