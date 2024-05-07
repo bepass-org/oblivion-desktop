bepass-org/oblivion-desktop

commit 92b378776f2259fcf24d0700ddec378e9584c8b8

fix: remove stuff on startup

src/main/ipcListeners/wp.ts
@@ -1,13 +1,12 @@
-/* eslint-disable no-unused-expressions */
 // warp-plus
 
-import { app, ipcMain } from 'electron';
+import { ipcMain } from 'electron';
 import treeKill from 'tree-kill';
 import path from 'path';
 import settings from 'electron-settings';
 import { countries, defaultSettings } from '../../defaultSettings';
 import { appendToLogFile, wpLogPath, writeToLogFile } from '../lib/log';
-import { doesFileExist, isDev } from '../lib/utils';
+import { doesFileExist, wpDirPath, wpFileName } from '../lib/utils';
 import { disableProxy, enableProxy } from '../lib/proxy';
 
 const { spawn } = require('child_process');
@@ -100,15 +99,6 @@ ipcMain.on('wp-start', async (event, arg) => {
     ) {
         await enableProxy();
     }
-
-    const wpFileName = `warp-plus${process.platform === 'win32' ? '.exe' : ''}`;
-    const wpDirPath = isDev()
-        ? path.join(
-            app.getAppPath().replace('/app.asar', '').replace('\\app.asar', ''),
-            'assets',
-            'bin'
-        )
-        : path.join(app.getPath('temp'));
 
     const command = path.join(wpDirPath, wpFileName);
 

src/main/lib/utils.ts
@@ -1,3 +1,6 @@
+import { app } from 'electron';
+import path from 'path';
+
 const fs = require('fs');
 
 export const isDev = () => process.env.NODE_ENV === 'development';
@@ -20,21 +23,40 @@ export const doesDirectoryExist = doesFileExist;
 export const doesFolderExist = doesFileExist;
 
 export function removeFileIfExists(filePath: string) {
-    return new Promise((resolve, reject) => {
-        fs.access(filePath, fs.constants.F_OK, (err: any) => {
-            if (err && err.code === 'ENOENT') {
-                resolve(false);
-            } else if (err) {
-                reject(err);
-            } else {
-                fs.unlink(filePath, (err2: any) => {
-                    if (err2) {
-                        reject(err2);
-                    } else {
-                        resolve(true);
-                    }
-                });
-            }
-        });
+    return new Promise(async (resolve, reject) => {
+        if (await doesFileExist(filePath)) {
+            fs.unlink(filePath, (err2: any) => {
+                if (err2) {
+                    reject(err2);
+                } else {
+                    resolve(true);
+                }
+            });
+        }
     });
 }
+
+export function removeDirIfExists(dirPath: string) {
+    return new Promise(async (resolve, reject) => {
+        if (await doesDirectoryExist(dirPath)) {
+            fs.rm(dirPath, { recursive: true, force: true }, (err2: any) => {
+                if (err2) {
+                    reject(err2);
+                } else {
+                    resolve(true);
+                }
+            });
+        }
+    });
+}
+
+export const wpFileName = `warp-plus${process.platform === 'win32' ? '.exe' : ''}`;
+export const wpDirPath = isDev()
+    ? path.join(
+          app.getAppPath().replace('/app.asar', '').replace('\\app.asar', ''),
+          'assets',
+          'bin'
+      )
+    : path.join(app.getPath('temp'));
+
+export const stuffPath = path.join(wpDirPath, 'stuff');

src/main/main.ts
@@ -14,7 +14,7 @@ import { app, BrowserWindow, ipcMain, screen, shell, Menu, Tray } from 'electron
 import settings from 'electron-settings';
 import MenuBuilder from './menu';
 import './ipc';
-import { isDev, removeFileIfExists } from './lib/utils';
+import { isDev, removeDirIfExists, removeFileIfExists, stuffPath } from './lib/utils';
 import { openDevToolsByDefault, useCustomWindowXY } from './dxConfig';
 import { disableProxy } from './lib/proxy';
 import { wpLogPath } from './lib/log';
@@ -59,6 +59,7 @@ function resolveHtmlPath(htmlFileName: string) {
 }
 
 removeFileIfExists(wpLogPath);
+removeDirIfExists(stuffPath);
 
 const isDebug = process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 

commit 6f7592b2e1f4b624afac62d54309d5269fa00995

🐛 fix: undefined bug caused by 92b378776f2259fcf24d0700ddec378e9584c8b8

src/main/ipcListeners/wp.ts
@@ -1,12 +1,13 @@
+/* eslint-disable no-unused-expressions */
 // warp-plus
 
-import { ipcMain } from 'electron';
+import { app, ipcMain } from 'electron';
 import treeKill from 'tree-kill';
 import path from 'path';
 import settings from 'electron-settings';
 import { countries, defaultSettings } from '../../defaultSettings';
 import { appendToLogFile, wpLogPath, writeToLogFile } from '../lib/log';
-import { doesFileExist, wpDirPath, wpFileName } from '../lib/utils';
+import { doesFileExist, isDev } from '../lib/utils';
 import { disableProxy, enableProxy } from '../lib/proxy';
 
 const { spawn } = require('child_process');
@@ -17,6 +18,15 @@ const randomCountry = () => {
     const randomIndex = Math.floor(Math.random() * countries.length);
     return countries[randomIndex]?.value ? countries[randomIndex]?.value : 'DE';
 };
+
+export const wpFileName = `warp-plus${process.platform === 'win32' ? '.exe' : ''}`;
+export const wpDirPath = isDev()
+    ? path.join(
+          app.getAppPath().replace('/app.asar', '').replace('\\app.asar', ''),
+          'assets',
+          'bin'
+      )
+    : path.join(app.getPath('temp'));
 
 // ! make sure you get the args like ({ port = '' })
 const wpErrorTranslation: any = {
@@ -44,12 +54,12 @@ const wpErrorTranslation: any = {
     'object null is not iterable': () => {
         return `برنامه با خطا مواجه شد؛ مجدداً تلاش کنید.`;
     },
-    'powershell': () => {
+    powershell: () => {
         return `برنامه برای اجرا به نصب نرم‌افزار Powershell نیاز دارد.`;
     }
 };
 
-ipcMain.on('wp-start', async (event) => {
+ipcMain.on('wp-start', async (event, arg) => {
     // in case user is using another proxy
     // await disableProxy();
 

src/main/lib/utils.ts
@@ -1,6 +1,3 @@
-import { app } from 'electron';
-import path from 'path';
-
 const fs = require('fs');
 
 export const isDev = () => process.env.NODE_ENV === 'development';
@@ -49,14 +46,3 @@ export function removeDirIfExists(dirPath: string) {
         }
     });
 }
-
-export const wpFileName = `warp-plus${process.platform === 'win32' ? '.exe' : ''}`;
-export const wpDirPath = isDev()
-    ? path.join(
-          app.getAppPath().replace('/app.asar', '').replace('\\app.asar', ''),
-          'assets',
-          'bin'
-      )
-    : path.join(app.getPath('temp'));
-
-export const stuffPath = path.join(wpDirPath, 'stuff');

src/main/main.ts
@@ -14,12 +14,15 @@ import { app, BrowserWindow, ipcMain, screen, shell, Menu, Tray } from 'electron
 import settings from 'electron-settings';
 import MenuBuilder from './menu';
 import './ipc';
-import { isDev, removeFileIfExists } from './lib/utils';
+import { isDev, removeDirIfExists, removeFileIfExists } from './lib/utils';
 import { openDevToolsByDefault, useCustomWindowXY } from './dxConfig';
 import { disableProxy } from './lib/proxy';
 import { wpLogPath } from './lib/log';
+import { wpDirPath, wpFileName } from './ipcListeners/wp';
 
 let mainWindow: BrowserWindow | null = null;
+
+export const stuffPath = path.join(wpDirPath, 'stuff');
 
 // console.log(1, app.getPath('appData'));
 // console.log(2, app.getPath('logs'));
@@ -29,7 +32,6 @@ let mainWindow: BrowserWindow | null = null;
 
 // coping wp binary to tmp on production so it can run without sudo/administrator privilege
 if (!isDev()) {
-    const wpFileName = `warp-plus${process.platform === 'win32' ? '.exe' : ''}`;
     const source = path.join(
         app.getAppPath().replace('/app.asar', '').replace('\\app.asar', ''),
         'assets',
@@ -59,6 +61,7 @@ function resolveHtmlPath(htmlFileName: string) {
 }
 
 removeFileIfExists(wpLogPath);
+removeDirIfExists(stuffPath);
 
 const isDebug = process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 