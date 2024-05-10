bacbb215d95e4f3f5ae62f0c9c934176fab07ec4
IRCF <ircfspace@gmail.com>
2024-05-10 13:07:46 +0330

    i18n: smallFix

--- src/renderer/store.ts
+++ src/renderer/store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { getLang } from './lib/loaders';
 
 export interface IStore {
     isConnected: boolean;
@@ -9,11 +10,12 @@ export interface IStore {
     setStatusText: any;
 }
 
+const appLang = getLang();
 export const useStore = create<IStore>((set) => ({
     isConnected: false,
     setIsConnected: (bool: boolean) => set(() => ({ isConnected: bool })),
     isLoading: false,
     setIsLoading: (bool: boolean) => set(() => ({ isLoading: bool })),
-    statusText: 'متصل نیستید',
+    statusText: appLang?.status?.disconnected,
     setStatusText: (status: string) => set(() => ({ statusText: status }))
 }));
