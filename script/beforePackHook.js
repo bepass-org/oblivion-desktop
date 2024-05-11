4fece7d0f616929060e12a62cc60a0ef682f29df
kiomarzsss <kiomarzsss@gmail.com>
2024-05-11 13:11:56 -0400

    build: hopfully fixes downloding correct warp-plus binary based on platform and arch

--- script/beforePackHook.js
+++ script/beforePackHook.js
@@ -2,9 +2,11 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 exports.default = async function (context) {
-    console.log(context);
+    // console.log(context);
     // TODO don't force download when packaging for the local platform
-    const { stdout, stderr } = await exec('npm exec ts-node script/dlWp.ts force');
+    const { stdout, stderr } = await exec(
+        `npm exec ts-node script/dlWp.ts force ${context.electronPlatformName} ${context.arch === '1' ? 'x64' : 'arm64'}`
+    );
 
     if (stderr) {
         console.error(stderr);
