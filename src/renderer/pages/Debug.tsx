d9e9ad25481b8a2e95c2bf44503b60f76c30ceb3
IRCF <ircfspace@gmail.com>
2024-05-13 09:30:30 +0330

    حذف برخی از جزییات در لاگ

--- src/renderer/pages/Debug.tsx
+++ src/renderer/pages/Debug.tsx
@@ -16,19 +16,23 @@ export default function Debug() {
         const intervalId = setInterval(() => {
             ipcRenderer.sendMessage('log');
         }, 1000);
-
         // Cleanup function to clear the interval
         return () => clearInterval(intervalId);
     }, []);
 
     ipcRenderer.on('log', (data) => {
-        setLog(String(data));
+        let updatedData = String(data);
+        updatedData = updatedData.replace(/([A-Z]):\\Users\\[^\\]+\\/g, '<DRIVE>:\\Users\\<USER>\\');
+        updatedData = updatedData.replace(/([A-Z]):\\/g, '<DRIVE>:\\');
+        updatedData = updatedData.replace(/\/home\/[^\\]+\//, 'home/<USER>/');
+        updatedData = updatedData.replace(/\\www\\[^\\]+\\/, '\\www\\<DIR>\\');
+        updatedData = updatedData.replace(/\\htdocs\\[^\\]+\\/, '\\www\\<DIR>\\');
+        setLog(updatedData);
     });
 
     const handleCopy = (e: { preventDefault: () => void }, value: any) => {
         e.preventDefault();
         navigator.clipboard.writeText(value);
-
         defaultToast(`${appLang?.toast?.copied}`, 'COPIED', 2000);
     };
 
