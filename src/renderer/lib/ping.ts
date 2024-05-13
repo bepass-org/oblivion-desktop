47a44e8b2c5fcc6ca615702ffd2bb8df299a9269
IRCF <ircfspace@gmail.com>
2024-05-13 20:14:32 +0330

    Create ping.ts

---
+++ src/renderer/lib/ping.ts
@@ -0,0 +1,16 @@
+export const ping = async () => {
+    try {
+        const started = window.performance.now();
+        const http = new XMLHttpRequest();
+        await http.open('GET', 'http://cp.cloudflare.com', true);
+        http.onreadystatechange = function() {
+        };
+        http.onloadend = function(e) {
+            const milliseconds = Math.round(window.performance.now() - started);
+            console.log(milliseconds);
+        };
+        http.send();
+    } catch (error) {
+        return false;
+    }
+};
