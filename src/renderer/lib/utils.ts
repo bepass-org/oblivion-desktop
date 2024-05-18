8c860f4f3c4d038af14dc68444a4ce6ea0f93fea
kiomarzsss <kiomarzsss@gmail.com>
2024-05-18 03:53:05 -0700

    🔒️ improve hiding username

--- src/renderer/lib/utils.ts
+++ src/renderer/lib/utils.ts
@@ -1,4 +1,4 @@
-export const { ipcRenderer, platform, NODE_ENV } = window.electron;
+export const { ipcRenderer, platform, NODE_ENV, username } = window.electron;
 
 export const isDev = () => window.electron.NODE_ENV === 'development';
 
