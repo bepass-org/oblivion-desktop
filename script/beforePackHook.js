a23936c24cc4d29e34cdee9b1915c27af5bb8a00
kiomarzsss <kiomarzsss@gmail.com>
2024-05-28 14:14:28 -0400

    🔖 0.9.7-beta

--- script/beforePackHook.js
+++ script/beforePackHook.js
@@ -2,8 +2,10 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 exports.default = async function (context) {
+    console.log('🚀 ~ file: beforePackHook.js:12 ~ context.arch:', context.arch);
     const archDict = {
         1: 'x64',
+        2: 'ia32',
         3: 'arm64'
     };
     // TODO don't force download when packaging on the local platform
