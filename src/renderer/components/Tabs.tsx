6bd9bfcd8ff36aad4ea7eba4b46876364cce2469
Arsham <arsham.ramezani@gmail.com>
2024-05-30 10:16:27 -0700

    perf: unused fragment tag of tabs component removed

--- src/renderer/components/Tabs.tsx
+++ src/renderer/components/Tabs.tsx
@@ -2,44 +2,46 @@ import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import { getLang } from '../lib/loaders';
 
-export default function Tabs({ active }: { active: string }) {
+interface TabsProps {
+    active: string;
+}
+
+export default function Tabs({ active }: TabsProps) {
     const appLang = getLang();
     return (
-        <>
-            <div className={classNames('tabs', 'inSettings')}>
-                <ul>
-                    <li>
-                        <Link to={'/'}>
-                            <i className={'material-icons'}>&#xe9f6;</i>
-                            <span>{appLang?.tabs?.home}</span>
-                        </Link>
-                    </li>
-                    <li className={active === 'settings' ? 'active' : ''}>
-                        <Link to={'/settings'}>
-                            <i className={'material-icons'}>&#xe429;</i>
-                            <span>{appLang?.tabs?.warp}</span>
-                        </Link>
-                    </li>
-                    <li className={active === 'network' ? 'active' : ''}>
-                        <Link to={'/network'}>
-                            <i className={'material-icons'}>&#xeb2f;</i>
-                            <span>{appLang?.tabs?.network}</span>
-                        </Link>
-                    </li>
-                    <li className={active === 'scanner' ? 'active' : ''}>
-                        <Link to={'/scanner'}>
-                            <i className={'material-icons'}>&#xe2db;</i>
-                            <span>{appLang?.tabs?.scanner}</span>
-                        </Link>
-                    </li>
-                    <li className={active === 'options' ? 'active' : ''}>
-                        <Link to={'/options'}>
-                            <i className={'material-icons'}>&#xe8b8;</i>
-                            <span>{appLang?.tabs?.app}</span>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </>
+        <div className={classNames('tabs', 'inSettings')}>
+            <ul>
+                <li>
+                    <Link to={'/'}>
+                        <i className={'material-icons'}>&#xe9f6;</i>
+                        <span>{appLang?.tabs?.home}</span>
+                    </Link>
+                </li>
+                <li className={active === 'settings' ? 'active' : ''}>
+                    <Link to={'/settings'}>
+                        <i className={'material-icons'}>&#xe429;</i>
+                        <span>{appLang?.tabs?.warp}</span>
+                    </Link>
+                </li>
+                <li className={active === 'network' ? 'active' : ''}>
+                    <Link to={'/network'}>
+                        <i className={'material-icons'}>&#xeb2f;</i>
+                        <span>{appLang?.tabs?.network}</span>
+                    </Link>
+                </li>
+                <li className={active === 'scanner' ? 'active' : ''}>
+                    <Link to={'/scanner'}>
+                        <i className={'material-icons'}>&#xe2db;</i>
+                        <span>{appLang?.tabs?.scanner}</span>
+                    </Link>
+                </li>
+                <li className={active === 'options' ? 'active' : ''}>
+                    <Link to={'/options'}>
+                        <i className={'material-icons'}>&#xe8b8;</i>
+                        <span>{appLang?.tabs?.app}</span>
+                    </Link>
+                </li>
+            </ul>
+        </div>
     );
 }
