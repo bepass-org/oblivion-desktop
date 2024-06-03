d07b2a584f7f3d9a15103f23b49c5285872e8a61
Ghost <ghostbye.by@gmail.com>
2024-06-03 02:02:05 -0700

    perf: code debug page made cleaner

--- src/renderer/pages/Debug/index.tsx
+++ src/renderer/pages/Debug/index.tsx
@@ -1,108 +1,11 @@
-import { KeyboardEvent, MouseEvent, useCallback, useEffect, useRef, useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import classNames from 'classnames';
-import Nav from '../components/Nav';
-import { ipcRenderer, username } from '../lib/utils';
-import { defaultToast } from '../lib/toasts';
-import { getLang } from '../lib/loaders';
-import useGoBackOnEscape from '../hooks/useGoBackOnEscape';
+import Nav from '../../components/Nav';
+import useDebug from './useDebug';
 
 export default function Debug() {
-    const [log, setLog] = useState<string>('');
-    const [autoScroll, setAutoScroll] = useState<boolean>(false);
-    const logRef = useRef<HTMLParagraphElement>(null);
-    //const [isBottom, setIsBottom] = useState(true);
-    const appLang = getLang();
 
-    // asking for log every 1.5sec
-    useEffect(() => {
-        ipcRenderer.sendMessage('getLogs');
-        const intervalId = setInterval(() => {
-            ipcRenderer.sendMessage('getLogs');
-        }, 1500);
-        // Cleanup function to clear the interval
-        return () => clearInterval(intervalId);
-    }, []);
-
-    useEffect(() => {
-        if (autoScroll) {
-            logRef?.current?.scrollIntoView({
-                behavior: 'smooth',
-                block: 'end'
-            });
-        }
-    }, [autoScroll]);
-
-    useEffect(() => {
-        if (autoScroll) {
-            logRef?.current?.scrollIntoView({
-                behavior: 'smooth',
-                block: 'end'
-            });
-        }
-    }, [log]);
-
-    useGoBackOnEscape();
-
-    const userFlag = '<USERNAME>';
-    ipcRenderer.on('getLogs', (data) => {
-        let logs = String(data);
-        // protect user privacy
-        // @ts-ignore
-        logs = logs.replaceAll(username, userFlag);
-        logs = logs.replaceAll(/\\\\/g, '\\');
-        // updatedData = updatedData.replace(/([A-Z]):\\/g, '<DRIVE>:\\');
-        // updatedData = updatedData.replace(/\/home\/[^\\]+\//, 'home/<USER>/');
-        // updatedData = updatedData.replace(/\\www\\[^\\]+\\/, '\\www\\<DIR>\\');
-        // updatedData = updatedData.replace(/\\htdocs\\[^\\]+\\/, '\\www\\<DIR>\\');
-        setLog(logs);
-    });
-
-    const handleCopy = useCallback(
-        (event: MouseEvent<HTMLElement> | KeyboardEvent<HTMLDivElement>) => {
-            event.preventDefault();
-            navigator.clipboard.writeText(log);
-            defaultToast(`${appLang?.toast?.copied}`, 'COPIED', 2000);
-        },
-        [appLang?.toast?.copied, log]
-    );
-
-    const handleKeyDown = useCallback(
-        (event: KeyboardEvent<HTMLDivElement>) => {
-            if (event.key === 'Enter') {
-                handleCopy(event);
-            }
-        },
-        [handleCopy]
-    );
-
-    const setAuthScrollEnabled = useCallback(() => setAutoScroll(true), []);
-
-    const setAuthScrollDisabled = useCallback(() => setAutoScroll(false), []);
-
-    // const handleClearLog = (e: { preventDefault: () => void }) => {
-    //     e.preventDefault();
-    //     defaultToast(`${appLang?.toast?.cleared}`, 'CLEARED', 2000);
-    // };
-
-    /*const onScroll = () => {
-        const isNearBottom =
-            Math.ceil(window.innerHeight + window.scrollY + 200) >=
-            document.documentElement.scrollHeight;
-        if (!isNearBottom) {
-            setIsBottom(true);
-        } else {
-            setIsBottom(false);
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', onScroll);
-        return () => {
-            window.removeEventListener('scroll', onScroll);
-        };
-    }, []);*/
-
+    const {appLang,autoScroll,handleCopy,handleKeyDown,log,logRef,setAuthScrollDisabled,setAuthScrollEnabled}=useDebug()
     return (
         <>
             <Nav title={appLang?.log?.title} />
@@ -127,7 +30,7 @@ export default function Debug() {
                                         <div
                                             onClick={setAuthScrollDisabled}
                                             role='presentation'
-                                            tabIndex={0}
+                                            tabIndex={-1}
                                         >
                                             <i className='material-icons' role='link'>
                                                 &#xe1a2;
@@ -138,7 +41,7 @@ export default function Debug() {
                                     <div
                                         role='presentation'
                                         onClick={setAuthScrollEnabled}
-                                        tabIndex={0}
+                                        tabIndex={-1}
                                     >
                                         <i className='material-icons' role='link'>
                                             &#xe038;
