382fa9175863cc34ffd6f02ef52bf15061dcc0cb
kiomarzsss <kiomarzsss@gmail.com>
2024-06-05 11:21:02 -0700

    ⚰️ remove unused code

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -1,11 +1,6 @@
-import { Menu, shell, BrowserWindow, MenuItemConstructorOptions } from 'electron';
+import { Menu, shell, BrowserWindow } from 'electron';
 import { exitTheApp } from './lib/utils';
 import { regeditVbsDirPath } from './main';
-
-interface DarwinMenuItemConstructorOptions extends MenuItemConstructorOptions {
-    selector?: string;
-    submenu?: DarwinMenuItemConstructorOptions[] | Menu;
-}
 
 export default class MenuBuilder {
     mainWindow: BrowserWindow;
@@ -19,10 +14,7 @@ export default class MenuBuilder {
             this.setupDevelopmentEnvironment();
         }
 
-        const template =
-            process.platform === 'darwin'
-                ? this.buildDarwinTemplate()
-                : this.buildDefaultTemplate();
+        const template = this.buildDefaultTemplate();
 
         const menu = Menu.buildFromTemplate(template);
         Menu.setApplicationMenu(menu);
@@ -45,166 +37,12 @@ export default class MenuBuilder {
         });
     }
 
-    buildDarwinTemplate(): MenuItemConstructorOptions[] {
-        const subMenuAbout: DarwinMenuItemConstructorOptions = {
-            label: 'Electron',
-            submenu: [
-                {
-                    label: 'About ElectronReact',
-                    selector: 'orderFrontStandardAboutPanel:'
-                },
-                { type: 'separator' },
-                { label: 'Services', submenu: [] },
-                { type: 'separator' },
-                {
-                    label: 'Hide ElectronReact',
-                    accelerator: 'Command+H',
-                    selector: 'hide:'
-                },
-                {
-                    label: 'Hide Others',
-                    accelerator: 'Command+Shift+H',
-                    selector: 'hideOtherApplications:'
-                },
-                { label: 'Show All', selector: 'unhideAllApplications:' },
-                { type: 'separator' },
-                {
-                    label: 'Exit',
-                    accelerator: 'Command+Q'
-                    // click: () => {
-                    // }
-                }
-            ]
-        };
-        const subMenuEdit: DarwinMenuItemConstructorOptions = {
-            label: 'Edit',
-            submenu: [
-                { label: 'Undo', accelerator: 'Command+Z', selector: 'undo:' },
-                {
-                    label: 'Redo',
-                    accelerator: 'Shift+Command+Z',
-                    selector: 'redo:'
-                },
-                { type: 'separator' },
-                { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' },
-                { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' },
-                {
-                    label: 'Paste',
-                    accelerator: 'Command+V',
-                    selector: 'paste:'
-                },
-                {
-                    label: 'Select All',
-                    accelerator: 'Command+A',
-                    selector: 'selectAll:'
-                }
-            ]
-        };
-        const subMenuViewDev: MenuItemConstructorOptions = {
-            label: 'View',
-            submenu: [
-                {
-                    label: 'Reload',
-                    accelerator: 'Command+R',
-                    click: () => {
-                        this.mainWindow.webContents.reload();
-                    }
-                },
-                {
-                    label: 'Toggle Full Screen',
-                    accelerator: 'Ctrl+Command+F',
-                    click: () => {
-                        this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
-                    }
-                },
-                {
-                    label: 'Toggle Developer Tools',
-                    accelerator: 'Alt+Command+I',
-                    click: () => {
-                        this.mainWindow.webContents.toggleDevTools();
-                    }
-                }
-            ]
-        };
-        const subMenuViewProd: MenuItemConstructorOptions = {
-            label: 'View',
-            submenu: [
-                {
-                    label: 'Toggle Full Screen',
-                    accelerator: 'Ctrl+Command+F',
-                    click: () => {
-                        this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
-                    }
-                }
-            ]
-        };
-        const subMenuWindow: DarwinMenuItemConstructorOptions = {
-            label: 'Window',
-            submenu: [
-                {
-                    label: 'Minimize',
-                    accelerator: 'Command+M',
-                    selector: 'performMiniaturize:'
-                },
-                {
-                    label: 'Close',
-                    accelerator: 'Command+W',
-                    selector: 'performClose:'
-                },
-                { type: 'separator' },
-                { label: 'Bring All to Front', selector: 'arrangeInFront:' }
-            ]
-        };
-        const subMenuHelp: MenuItemConstructorOptions = {
-            label: 'Help',
-            submenu: [
-                {
-                    label: 'Learn More',
-                    click() {
-                        shell.openExternal('https://electronjs.org');
-                    }
-                },
-                {
-                    label: 'Documentation',
-                    click() {
-                        shell.openExternal(
-                            'https://github.com/electron/electron/tree/main/docs#readme'
-                        );
-                    }
-                },
-                {
-                    label: 'Community Discussions',
-                    click() {
-                        shell.openExternal('https://www.electronjs.org/community');
-                    }
-                },
-                {
-                    label: 'Search Issues',
-                    click() {
-                        shell.openExternal('https://github.com/electron/electron/issues');
-                    }
-                }
-            ]
-        };
-
-        const subMenuView =
-            process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true'
-                ? subMenuViewDev
-                : subMenuViewProd;
-
-        return [subMenuAbout, subMenuEdit, subMenuView, subMenuWindow, subMenuHelp];
-    }
-
     buildDefaultTemplate() {
         const templateDefault = [
             {
                 // TODO locale
                 label: '&File',
                 submenu: [
-                    // {
-                    //     label: '&Open',
-                    //     accelerator: 'Ctrl+O'
-                    // },
                     {
                         // TODO locale
                         label: '&Exit',
@@ -215,80 +53,15 @@ export default class MenuBuilder {
                     }
                 ]
             },
-            // {
-            //     label: '&View',
-            //     submenu:
-            //         process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true'
-            //             ? [
-            //                   {
-            //                       label: '&Reload',
-            //                       accelerator: 'Ctrl+R',
-            //                       click: () => {
-            //                           this.mainWindow.webContents.reload();
-            //                       }
-            //                   },
-            //                   {
-            //                       label: 'Toggle &Full Screen',
-            //                       accelerator: 'F11',
-            //                       click: () => {
-            //                           this.mainWindow.setFullScreen(
-            //                               !this.mainWindow.isFullScreen()
-            //                           );
-            //                       }
-            //                   },
-            //                   {
-            //                       label: 'Toggle &Developer Tools',
-            //                       accelerator: 'Alt+Ctrl+I',
-            //                       click: () => {
-            //                           this.mainWindow.webContents.toggleDevTools();
-            //                       }
-            //                   }
-            //               ]
-            //             : [
-            //                   {
-            //                       label: 'Toggle &Full Screen',
-            //                       accelerator: 'F11',
-            //                       click: () => {
-            //                           this.mainWindow.setFullScreen(
-            //                               !this.mainWindow.isFullScreen()
-            //                           );
-            //                       }
-            //                   }
-            //               ]
-            // },
             {
                 // TODO locale
                 label: 'Help',
                 submenu: [
                     {
                         // TODO locale
-
                         label: 'Learn More',
                         click() {
                             shell.openExternal('https://github.com/bepass-org/oblivion-desktop');
-                        }
-                    },
-                    // {
-                    //     label: 'Documentation',
-                    //     click() {
-                    //         shell.openExternal(
-                    //             'https://github.com/electron/electron/tree/main/docs#readme'
-                    //         );
-                    //     }
-                    // },
-                    // {
-                    //     label: 'Community Discussions',
-                    //     click() {
-                    //         shell.openExternal('https://www.electronjs.org/community');
-                    //     }
-                    // },
-                    {
-                        // TODO locale
-                        label: 'Issues',
-                        click() {
-                            shell.openExternal(
-                                'https://github.com/bepass-org/oblivion-desktop/issues'
-                            );
                         }
                     }
                 ]
