1c6ff549ac043b3d3b20159163847f459399e00e
kiomarzsss <kiomarzsss@gmail.com>
2024-06-10 12:56:23 -0400

    🎨 code format

--- src/renderer/lib/toasts.tsx
+++ src/renderer/lib/toasts.tsx
@@ -17,7 +17,7 @@ export const defaultToast = (msg = '', id = 'ID', duration = 5000) => {
     });
     setTimeout(() => {
         toast.remove(id);
-    }, (duration+200));
+    }, duration + 200);
 };
 
 export const defaultToastWithSubmitButton = (
