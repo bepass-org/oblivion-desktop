eb0577615f351923fedd79cb4fd5f906a6d6f88f
IRCF <ircfspace@gmail.com>
2024-06-15 10:44:06 +0330

    feat: Add contextMenu for these inputs

---
+++ src/renderer/components/Input/index.tsx
@@ -0,0 +1,139 @@
+import React, { ChangeEvent, FC, useState, useEffect, useRef } from 'react';
+
+interface InputProps {
+    id?: string;
+    value: string;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    disabled?: boolean;
+    tabIndex?: number;
+}
+
+const Input: FC<InputProps> = ({ id, onChange, value, disabled, tabIndex = 0 }) => {
+    const [contextMenuStyle, setContextMenuStyle] = useState<{ left: number; top: number } | null>(
+        null
+    );
+    const inputRef = useRef<any>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (inputRef.current && !inputRef.current.contains(event.target as Node)) {
+                setContextMenuStyle(null);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
+    const handleContextMenu = (event: React.MouseEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        event.stopPropagation();
+        /*const inputElement = event.currentTarget;
+        inputElement.select();*/
+
+        const positionX = event.clientX;
+        const positionY = event.clientY;
+        const menuWidth = 100;
+        const menuHeight = 100;
+        const maxX = window.innerWidth - menuWidth;
+        const maxY = window.innerHeight - menuHeight;
+        const left = Math.min(positionX, maxX);
+        const top = Math.min(positionY, maxY);
+
+        setContextMenuStyle({ left, top });
+    };
+
+    const handleCopy = async () => {
+        try {
+            const selectedText = inputRef.current?.value.substring(
+                inputRef?.current.selectionStart,
+                inputRef.current.selectionEnd
+            );
+            if (selectedText) {
+                await navigator.clipboard.writeText(selectedText);
+            }
+        } catch (err) {
+            console.error('Failed to copy:', err);
+        }
+        setContextMenuStyle(null);
+    };
+
+    const handleCut = async () => {
+        try {
+            const selectedText = inputRef.current?.value.substring(
+                inputRef.current.selectionStart,
+                inputRef.current.selectionEnd
+            );
+            if (selectedText) {
+                await navigator.clipboard.writeText(selectedText);
+                const newValue =
+                    value.substring(0, inputRef.current.selectionStart) +
+                    value.substring(inputRef.current.selectionEnd);
+                onChange({ target: { value: newValue } } as ChangeEvent<HTMLInputElement>);
+            }
+        } catch (err) {
+            console.error('Failed to cut:', err);
+        }
+        setContextMenuStyle(null);
+    };
+
+    const handlePaste = async () => {
+        try {
+            const text = await navigator.clipboard.readText();
+            const selectionStart = inputRef.current.selectionStart;
+            const selectionEnd = inputRef.current.selectionEnd;
+            const newValue =
+                value.substring(0, selectionStart) + text + value.substring(selectionEnd);
+            onChange({ target: { value: newValue } } as ChangeEvent<HTMLInputElement>);
+        } catch (err) {
+            console.error('Failed to paste:', err);
+        }
+        setContextMenuStyle(null);
+    };
+
+    const handleCloseContextMenu = () => {
+        setContextMenuStyle(null);
+    };
+
+    return (
+        <>
+            <input
+                ref={inputRef}
+                id={id}
+                value={value}
+                tabIndex={tabIndex}
+                spellCheck={false}
+                className='form-control'
+                onChange={onChange}
+                disabled={disabled}
+                onContextMenu={handleContextMenu}
+                type='text'
+            />
+            {contextMenuStyle && (
+                <div
+                    className='contextMenu'
+                    style={{
+                        position: 'fixed',
+                        left: contextMenuStyle.left,
+                        top: contextMenuStyle.top
+                    }}
+                    onClick={handleCloseContextMenu}
+                >
+                    <div className='menuItem' onClick={handleCopy}>
+                        Copy
+                    </div>
+                    <div className='menuItem' onClick={handleCut}>
+                        Cut
+                    </div>
+                    <div className='menuItem' onClick={handlePaste}>
+                        Paste
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default Input;
