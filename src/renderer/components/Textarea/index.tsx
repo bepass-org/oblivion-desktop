bf1a47a7fa7fedc02841c51d2a1eba6fe7b689be
Ghost <ghostbye.by@gmail.com>
2024-06-23 15:04:50 -0700

    code of textarea component splited to two files

--- src/renderer/components/Textarea/index.tsx
+++ src/renderer/components/Textarea/index.tsx
@@ -1,4 +1,5 @@
-import React, { ChangeEvent, FC, useState, useEffect, useRef, useCallback } from 'react';
+import React, { ChangeEvent, FC } from 'react';
+import useTextarea from './useTextarea';
 
 interface InputProps {
     id?: string;
@@ -7,95 +8,16 @@ interface InputProps {
     tabIndex?: number;
 }
 
-type ContextMenuStyleType = {
-    left: number;
-    top: number;
-};
-
 const Textarea: FC<InputProps> = ({ id, onChange, value, tabIndex = 0 }) => {
-    const [contextMenuStyle, setContextMenuStyle] = useState<ContextMenuStyleType | null>(null);
-    const inputRef = useRef<HTMLTextAreaElement>(null);
-
-    useEffect(() => {
-        const handleClickOutside = (event: MouseEvent) => {
-            if (inputRef.current && !inputRef.current.contains(event.target as Node)) {
-                setContextMenuStyle(null);
-            }
-        };
-
-        document.addEventListener('click', handleClickOutside);
-        return () => {
-            document.removeEventListener('click', handleClickOutside);
-        };
-    }, []);
-
-    const handleContextMenu = useCallback((event: React.MouseEvent<HTMLTextAreaElement>) => {
-        event.preventDefault();
-        event.stopPropagation();
-        /*const inputElement = event.currentTarget;
-    inputElement.select();*/
-
-        const positionX = event.clientX;
-        const positionY = event.clientY;
-        const menuWidth = 100;
-        const menuHeight = 100;
-        const maxX = window.innerWidth - menuWidth;
-        const maxY = window.innerHeight - menuHeight;
-        const left = Math.min(positionX, maxX);
-        const top = Math.min(positionY, maxY);
-
-        setContextMenuStyle({ left, top });
-    }, []);
-
-    const handleCopy = useCallback(async () => {
-        try {
-            const selectedText = inputRef.current?.value.substring(
-                inputRef?.current.selectionStart,
-                inputRef.current.selectionEnd
-            );
-            if (selectedText) {
-                await navigator.clipboard.writeText(selectedText);
-            }
-        } catch (err) {
-            console.error('Failed to copy:', err);
-        }
-        setContextMenuStyle(null);
-    }, [inputRef]);
-
-    const handleCut = useCallback(async () => {
-        try {
-            const selectionStart = inputRef.current?.selectionStart || 0;
-            const selectionEnd = inputRef.current?.selectionEnd || 0;
-            const selectedText = inputRef.current?.value.substring(selectionStart, selectionEnd);
-            if (selectedText) {
-                await navigator.clipboard.writeText(selectedText);
-                const newValue = value.substring(0, selectionStart) + value.substring(selectionEnd);
-                onChange({ target: { value: newValue } } as ChangeEvent<HTMLTextAreaElement>);
-            }
-        } catch (err) {
-            console.error('Failed to cut:', err);
-        }
-        setContextMenuStyle(null);
-    }, [value, onChange, inputRef]);
-
-    const handlePaste = useCallback(async () => {
-        try {
-            const selectionStart = inputRef.current?.selectionStart || 0;
-            const selectionEnd = inputRef.current?.selectionEnd || 0;
-
-            const text = await navigator.clipboard.readText();
-            const newValue =
-                value.substring(0, selectionStart) + text + value.substring(selectionEnd);
-            onChange({ target: { value: newValue } } as ChangeEvent<HTMLTextAreaElement>);
-        } catch (err) {
-            console.error('Failed to paste:', err);
-        }
-        setContextMenuStyle(null);
-    }, [value, onChange, inputRef]);
-
-    const handleCloseContextMenu = useCallback(() => {
-        setContextMenuStyle(null);
-    }, []);
+    const {
+        contextMenuStyle,
+        handleCloseContextMenu,
+        handleContextMenu,
+        handleCopy,
+        handleCut,
+        handlePaste,
+        inputRef
+    } = useTextarea(value, onChange);
 
     return (
         <>
