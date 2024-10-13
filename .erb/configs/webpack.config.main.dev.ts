c1a52e41b8d82a444bf45b76c6ccb102d036b4b9
kiomarzsss <kiomarzsss@gmail.com>
2024-10-13 16:52:32 -0400

    build: final packages update

--- .erb/configs/webpack.config.main.dev.ts
+++ .erb/configs/webpack.config.main.dev.ts
@@ -13,51 +13,51 @@ import webpackPaths from './webpack.paths';
 // When an ESLint server is running, we can't set the NODE_ENV so we'll check if it's
 // at the dev webpack config is not accidentally run in a production environment
 if (process.env.NODE_ENV === 'production') {
-  checkNodeEnv('development');
+    checkNodeEnv('development');
 }
 
 const configuration: webpack.Configuration = {
-  devtool: 'inline-source-map',
+    devtool: 'inline-source-map',
 
-  mode: 'development',
+    mode: 'development',
 
-  target: 'electron-main',
+    target: 'electron-main',
 
-  entry: {
-    main: path.join(webpackPaths.srcMainPath, 'main.ts'),
-    preload: path.join(webpackPaths.srcMainPath, 'preload.ts'),
-  },
-
-  output: {
-    path: webpackPaths.dllPath,
-    filename: '[name].bundle.dev.js',
-    library: {
-      type: 'umd',
+    entry: {
+        main: path.join(webpackPaths.srcMainPath, 'main.ts'),
+        preload: path.join(webpackPaths.srcMainPath, 'preload.ts')
     },
-  },
 
-  plugins: [
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    new BundleAnalyzerPlugin({
-      analyzerMode: process.env.ANALYZE === 'true' ? 'server' : 'disabled',
-      analyzerPort: 8888,
-    }),
+    output: {
+        path: webpackPaths.dllPath,
+        filename: '[name].bundle.dev.js',
+        library: {
+            type: 'umd'
+        }
+    },
 
-    new webpack.DefinePlugin({
-      'process.type': '"browser"',
-    }),
-  ],
+    plugins: [
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        new BundleAnalyzerPlugin({
+            analyzerMode: process.env.ANALYZE === 'true' ? 'server' : 'disabled',
+            analyzerPort: 8888
+        }),
 
-  /**
-   * Disables webpack processing of __dirname and __filename.
-   * If you run the bundle in node.js it falls back to these values of node.js.
-   * https://github.com/webpack/webpack/issues/2010
-   */
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
+        new webpack.DefinePlugin({
+            'process.type': '"browser"'
+        })
+    ],
+
+    /**
+     * Disables webpack processing of __dirname and __filename.
+     * If you run the bundle in node.js it falls back to these values of node.js.
+     * https://github.com/webpack/webpack/issues/2010
+     */
+    node: {
+        __dirname: false,
+        __filename: false
+    }
 };
 
 export default merge(baseConfig, configuration);
--- .erb/scripts/link-modules.ts
+++ .erb/scripts/link-modules.ts
@@ -1,14 +1,13 @@
 import fs from 'fs';
 import webpackPaths from '../configs/webpack.paths';
 
-const { srcNodeModulesPath, appNodeModulesPath, erbNodeModulesPath } =
-  webpackPaths;
+const { srcNodeModulesPath, appNodeModulesPath, erbNodeModulesPath } = webpackPaths;
 
 if (fs.existsSync(appNodeModulesPath)) {
-  if (!fs.existsSync(srcNodeModulesPath)) {
-    fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath, 'junction');
-  }
-  if (!fs.existsSync(erbNodeModulesPath)) {
-    fs.symlinkSync(appNodeModulesPath, erbNodeModulesPath, 'junction');
-  }
+    if (!fs.existsSync(srcNodeModulesPath)) {
+        fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath, 'junction');
+    }
+    if (!fs.existsSync(erbNodeModulesPath)) {
+        fs.symlinkSync(appNodeModulesPath, erbNodeModulesPath, 'junction');
+    }
 }
