c0ff2b5168836ea4e9716537e3a32544f6207b61
IRCF <ircfspace@gmail.com>
2024-10-15 16:25:20 +0330

    Update singBox version

--- src/main/config.ts
+++ src/main/config.ts
@@ -1,4 +1,4 @@
 export const wpVersion = '1.2.4';
-export const sbVersion = '1.9.7';
+export const sbVersion = '1.10.0';
 export const helperVersion = '1.0.6';
 export const geoDBs = ['geoip.db', 'geosite.db', 'security.db', 'security-ip.db'];
