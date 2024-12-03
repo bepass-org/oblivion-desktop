ff871090c8fefc8eddf5ea92fa9cf875ca033fb6
ShadowZagrosDev <shzagdev@proton.me>
2024-12-03 06:24:10 +0330

    fix listening to helper

--- src/main/config.ts
+++ src/main/config.ts
@@ -1,4 +1,4 @@
 export const wpVersion = '1.2.4';
 export const sbVersion = '1.10.3';
-export const helperVersion = '1.1.1';
+export const helperVersion = '1.1.3';
 export const geoDBs = ['geoip.db', 'geosite.db', 'security.db', 'security-ip.db'];
