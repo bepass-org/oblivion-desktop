1968c1642bdb6bd94bcbfe96cc0a9b8f4a36c805
IRCF <ircfspace@gmail.com>
2024-12-15 18:48:29 +0330

    Placing stopLoadingToast in the correct position

--- src/renderer/pages/Landing/useLanding.ts
+++ src/renderer/pages/Landing/useLanding.ts
@@ -183,13 +183,15 @@ const useLanding = () => {
                 setIsLoading(true);
                 setIsConnected(false);
                 loadingToast(appLang.status.keep_trying);
+                setTimeout(function () {
+                    stopLoadingToast();
+                }, 3500);
             } else if (message === 'restarted') {
                 setIsLoading(false);
                 setIsConnected(true);
             } else if (message === 'exceeded') {
                 setIsLoading(false);
                 setIsConnected(false);
-                stopLoadingToast();
                 setTimeout(function () {
                     defaultToast(appLang.log.error_deadline_exceeded, 'EXCEEDED', 5000);
                 }, 2000);
