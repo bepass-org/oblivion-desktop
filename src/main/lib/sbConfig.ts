49638af36d76eda253bf9fe97b3d9dc81bcdcded
ShadowZagrosDev <shzagdev@proton.me>
2024-12-18 16:08:31 +0330

    Set strict_route to true in sbConfig

--- src/main/lib/sbConfig.ts
+++ src/main/lib/sbConfig.ts
@@ -102,6 +102,7 @@ export function createSbConfig(
                 mtu: mtu,
                 address: ['172.19.0.1/30', 'fdfe:dcba:9876::1/126'],
                 auto_route: true,
+                strict_route: true,
                 stack: 'mixed'
             }
         ],
