816aae180384ac897aa5e3d6be1770698a1ab82d
IRCF <ircfspace@gmail.com>
2024-12-21 01:52:50 +0330

    Rewrite the method responsible for invoking country flags after a successful connection

--- src/renderer/lib/cfFlag.ts
+++ src/renderer/lib/cfFlag.ts
@@ -1,81 +1,16 @@
-import xx from '../../../assets/img/flags/xx.svg';
-import ir from '../../../assets/img/flags/iran.svg';
-import au from '../../../assets/img/flags/au.svg';
-import at from '../../../assets/img/flags/at.svg';
-import be from '../../../assets/img/flags/be.svg';
-import bg from '../../../assets/img/flags/bg.svg';
-import br from '../../../assets/img/flags/br.svg';
-import ca from '../../../assets/img/flags/ca.svg';
-import hr from '../../../assets/img/flags/hr.svg';
-import ch from '../../../assets/img/flags/ch.svg';
-import cz from '../../../assets/img/flags/cz.svg';
-import de from '../../../assets/img/flags/de.svg';
-import dk from '../../../assets/img/flags/dk.svg';
-import ee from '../../../assets/img/flags/ee.svg';
-import es from '../../../assets/img/flags/es.svg';
-import fi from '../../../assets/img/flags/fi.svg';
-import fr from '../../../assets/img/flags/fr.svg';
-import gb from '../../../assets/img/flags/gb.svg';
-import hu from '../../../assets/img/flags/hu.svg';
-import ie from '../../../assets/img/flags/ie.svg';
-import ind from '../../../assets/img/flags/in.svg';
-import id from '../../../assets/img/flags/id.svg';
-import it from '../../../assets/img/flags/it.svg';
-import jp from '../../../assets/img/flags/jp.svg';
-import lv from '../../../assets/img/flags/lv.svg';
-import nl from '../../../assets/img/flags/nl.svg';
-import no from '../../../assets/img/flags/no.svg';
-import pl from '../../../assets/img/flags/pl.svg';
-import pt from '../../../assets/img/flags/pt.svg';
-import ro from '../../../assets/img/flags/ro.svg';
-import rs from '../../../assets/img/flags/rs.svg';
-import se from '../../../assets/img/flags/se.svg';
-import sg from '../../../assets/img/flags/sg.svg';
-import sk from '../../../assets/img/flags/sk.svg';
-import ua from '../../../assets/img/flags/ua.svg';
-import us from '../../../assets/img/flags/us.svg';
-
-const flagMap: { [key: string]: string } = {
-    ir,
-    au,
-    at,
-    be,
-    bg,
-    br,
-    ca,
-    hr,
-    ch,
-    cz,
-    de,
-    dk,
-    ee,
-    es,
-    fi,
-    fr,
-    gb,
-    hu,
-    ie,
-    id,
-    it,
-    jp,
-    lv,
-    nl,
-    no,
-    pl,
-    pt,
-    ro,
-    rs,
-    se,
-    sg,
-    sk,
-    ua,
-    us,
-    in: ind
-};
-
-export const cfFlag = (code: any): string => {
-    if (typeof code !== 'string') {
-        return xx;
+export const cfFlag = (code: string): string => {
+    const flagMapping: Record<string, string> = {
+        ir: 'iran',
+        in: 'ind'
+    };
+    const defaultFlag = require('../../../assets/img/flags/xx.svg').default;
+    if (!code || typeof code !== 'string') {
+        return defaultFlag;
     }
-    return flagMap[code.toLowerCase()] || xx;
+    const normalizedCode = flagMapping[code.toLowerCase()] || code.toLowerCase();
+    try {
+        return require(`../../../assets/img/flags/${normalizedCode}.svg`).default;
+    } catch {
+        return defaultFlag;
+    }
 };
