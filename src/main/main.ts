dc8b362ce23bbe17799ef905dc9a1b5574e7ff76
IRCF <128790947+ircfspace@users.noreply.github.com>
2025-04-07 09:19:52 +0330

    Minor change in the ready-to-show event

--- src/main/main.ts
+++ src/main/main.ts
@@ -267,11 +267,7 @@ class OblivionDesktop {
         this.state.mainWindow.on('ready-to-show', async () => {
             const startMinimized = await settings.get('startMinimized');
             if (typeof startMinimized === 'boolean' && startMinimized) {
-                try {
-                    this.state.mainWindow?.minimize();
-                } catch (err) {
-                    console.error('Error minimizing the window:', err);
-                }
+                this.state.mainWindow?.minimize();
             } else {
                 this.state.mainWindow?.show();
             }
