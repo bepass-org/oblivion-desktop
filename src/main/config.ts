b3fcfc3e1429fb4487784f1e6e20dd9726e0c77e
IRCF <128790947+ircfspace@users.noreply.github.com>
2025-06-04 16:05:34 +0330

    Warp-Plus core updated to version 1.2.6

--- src/main/config.ts
+++ src/main/config.ts
@@ -1,4 +1,4 @@
-export const wpVersion = '1.2.5';
+export const wpVersion = '1.2.6';
 export const helperVersion = '1.2.2';
 export const netStatsVersion = '1.0.2';
 export const proxyResetVersion = '1.2.14';
