4f2ead70437d4f3f42ae51c1f7055f8837afb014
IRCF <ircfspace@gmail.com>
2025-07-27 11:34:21 +0330

    chore: format codebase using npm run format

--- src/renderer/lib/isAnyUndefined.ts
+++ src/renderer/lib/isAnyUndefined.ts
@@ -1,6 +1,7 @@
 export function isAnyUndefined(...values: unknown[]): boolean {
     return values.some((value) => typeof value === 'undefined');
 }
+
 export function typeIsUndefined(value: unknown): boolean {
     return typeof value === 'undefined';
 }
