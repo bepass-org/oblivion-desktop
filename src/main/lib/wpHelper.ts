b45f5a4d1901c9fe5563c28c9d48aef9dbac76cd
IRCF <ircfspace@gmail.com>
2025-08-30 09:49:38 +0330

    Update wpHelper.ts

--- src/main/lib/wpHelper.ts
+++ src/main/lib/wpHelper.ts
@@ -92,10 +92,10 @@ export const getUserSettings = async () => {
                       : defaultSettings.endpoint
               ]),
         ...(typeof reserved === 'boolean' && !reserved ? ['--reserved', '0,0,0'] : []),
-        ...[
+        /*...[
             '--connect-timeout',
             typeof connectTimeout === 'string' ? connectTimeout : defaultSettings.connectTimeout
-        ],
+        ],*/
         ...(finalDns !== '' ? ['--dns', finalDns] : [])
     ];
 };
