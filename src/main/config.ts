b3db4946cb8957dc0943638495f014efb8684cd5
IRCF <ircfspace@gmail.com>
2025-09-06 11:08:27 +0330

    Updating Masque-Plus core to version 2.7.2

--- src/main/config.ts
+++ src/main/config.ts
@@ -2,4 +2,4 @@ export const wpVersion = '1.2.6';
 export const helperVersion = '1.2.2';
 export const netStatsVersion = '1.0.2';
 export const proxyResetVersion = '1.2.14';
-export const mpVersion = '2.3.0';
+export const mpVersion = '2.7.2';
