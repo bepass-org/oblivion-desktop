92f5dc157c8e3ca77f2cc4ce4cd7179259b82b17
Dr-Bad <58116609+Dr-Bad@users.noreply.github.com>
2025-09-28 15:26:19 +0330

    Optimize devtools key listener

--- src/renderer/lib/dx.ts
+++ src/renderer/lib/dx.ts
@@ -4,19 +4,10 @@ import { ipcRenderer } from './utils';
 
 export const openDevtoolsOnCtrlShiftI = () => {
     // open devtools on dev environment by ctrl+shift+i
-    let keysDown: any = {};
     window.addEventListener('keydown', function (event) {
-        keysDown[event.keyCode] = true;
         // Check if Ctrl, Shift, and I keys are down at the same time
-        if (keysDown[17] && keysDown[16] && keysDown[73]) {
+        if (event.ctrlKey && event.shiftKey && event.code == "KeyI") {
             ipcRenderer.sendMessage('open-devtools');
-            setTimeout(() => {
-                keysDown = {};
-            }, 0);
         }
-    });
-
-    window.addEventListener('keyup', function (event) {
-        delete keysDown[event.keyCode];
     });
 };
