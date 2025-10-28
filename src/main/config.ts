e379a1e337d48992e68502f25c8874ad3a716f5b
Dr-Bad <58116609+Dr-Bad@users.noreply.github.com>
2025-10-28 14:04:28 +0330

    Update oblivion-helper to v1.3.2

--- src/main/config.ts
+++ src/main/config.ts
@@ -1,5 +1,5 @@
 export const wpVersion = '1.2.6';
-export const helperVersion = '1.3.1';
+export const helperVersion = '1.3.2';
 export const netStatsVersion = '1.0.2';
 export const proxyResetVersion = '1.2.14';
 export const mpVersion = '2.8.0';
