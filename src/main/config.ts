87b735c8b8511e7494aec74845c6029717ce2c8c
IRCF <ircfspace@gmail.com>
2025-11-01 10:49:24 +0330

    Update masque-plus to v2.9.0

--- src/main/config.ts
+++ src/main/config.ts
@@ -2,4 +2,4 @@ export const wpVersion = '1.2.6';
 export const helperVersion = '1.3.2';
 export const netStatsVersion = '1.0.2';
 export const proxyResetVersion = '1.2.14';
-export const mpVersion = '2.8.0';
+export const mpVersion = '2.9.0';
